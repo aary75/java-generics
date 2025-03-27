664f60a618fb37d35d41dc1c3db8bc082e271350
Aaryan <aaryankgarg@gmail.com>
2025-03-27 21:55:14 +0530

    [Aaryan] ProductCategory interface

---
+++ src/com.bridgelabz-dynamicOnlineMarketplace/ProductCategory.java
@@ -0,0 +1,13 @@
+package com.bridgelabz-dynamicOnlineMarketplace
+
+  GNU nano 7.2                                                                                   ProductCategory.java                                                                                   Modified
+package com.bridgelabz-dynamicOnlineMarketplace
+
+// Base interface for all product categories
+interface ProductCategory {
+    String getCategoryName();
+    double getMinPrice();
+    double getMaxPrice();
+}
+
+
